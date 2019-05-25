18c23746444f26e4c273d1f19ee879e1a076bc46
tcsvr <1484552626@qq.com>
2019-05-25 11:09:49 +0800

    first

---
+++ ECShop_V4.0.0_UTF8/source/ecshop/h5/static/config.js
@@ -0,0 +1,10 @@
+const env = {  
+  'API_HOST': 'http://api.ecshop.com',
+  // 'API_HOST': 'http://api.ecshop.com',
+  'APP_NAME': 'Demo',
+  'APP_DESC': 'This is a  demo',
+  'APP_KEYWORDS': 'Demo, Shop',
+  'DEBUG': false,
+  'ENCRYPTED': false,
+  'VERSION': '1.0.0', // API版本号
+}
